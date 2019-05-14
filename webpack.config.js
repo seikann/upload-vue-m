9b4795466d36dc0f9081f2b7d59fda834ce01ad0
seikann <390811346@qq.com>
2019-05-14 15:22:16 +0800

    修复了一些 bug

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'upload-vue-m.js',
-    library: 'custom-ui', // 指定的就是你使用require时的模块名
+    library: 'seikann-ui', // 指定的就是你使用require时的模块名
     libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
