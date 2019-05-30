d9a472923085d435d4eb4bb7ceb50aadaafb984b
seikann <390811346@qq.com>
2019-05-30 16:35:59 +0800

    1.1.4

--- src/packages/seikann-upload.vue
+++ src/packages/seikann-upload.vue
@@ -190,13 +190,14 @@ export default {
     },
     convertBase64UrlToBlob(urlData) {
       const bytes = window.atob(urlData.split(',')[1]); // 去掉url的头，并转换为byte
+      const mime = urlData.split(',')[0].match(/:(.*?);/)[1]
       // 处理异常,将ascii码小于0的转换为大于0
       const ab = new ArrayBuffer(bytes.length);
       const ia = new Uint8Array(ab);
       for (let i = 0; i < bytes.length; i++) {
         ia[i] = bytes.charCodeAt(i);
       }
-      return new Blob([ab], {type: 'image/png'});
+      return new Blob([ab], { type: mime });
     }
   }
 }
